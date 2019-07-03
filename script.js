e5755e819f4d57e1c4c3e5a6dc5a5b659085a5bb
Ginés Sarmiento <gines.sarmiento@gmail.com>
2019-07-03 21:10:28 +0200

    crea los archivos

---
+++ script.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function() {
+	$(".even").css("list-style-type","square");
+	$("li").eq(6).css("height","10px");
+	$(".odd").css('text-transform', 'uppercase');
+	$('#murcia').insertAfter('#madrid');
+});
